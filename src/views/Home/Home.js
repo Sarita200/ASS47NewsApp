2a715731854c18ee2fca5ab30d7afe9f8f571d4d
Sarita200 <saritasadgir1@gmail.com>
2024-07-12 18:45:04 +0530

    basic structure of react file created

---
+++ src/views/Home/Home.js
@@ -0,0 +1,39 @@
+import React, {useEffect,useState }  from 'react'
+import './Home.css'
+import axios from "axios"
+
+function Home() {
+    const[news,setNews] = useState([])
+
+    const loadNews = async ()=>{
+        const response = await axios.get(`https://newsapi.org/v2/everything?q=apple&from=2024-07-10&to=2024-07-10&sortBy=popularity&apiKey=5f7108985bba4756b28c3119e306ba28`);
+
+        setNews(response.data.articles)
+    }
+    useEffect(() => {
+        loadNews()
+    } , [])
+
+  return (
+    <div>
+      <h1>News App</h1>
+
+      {
+        news.map((newsArticle, index) =>{
+    
+            const {author ,title, description, url, urlToImage ,publishedAt, content} = newsArticle
+
+            return(
+                <div>
+                    <img src={urlToImage} alt="" className='news-artical-img'/>
+                </div>
+            )
+            
+            }  )
+      }
+      
+    </div>
+  )
+}
+
+export default Home
